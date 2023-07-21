f2d5cac61fe0ff8fe67db46b07540cd4c6cf1d66
dingdong <1428837023@qq.com>
2023-07-21 17:46:58 +0800

    3.6.2 另起端口监听 HTTP

--- main.go
+++ main.go
@@ -8,27 +8,79 @@ import (
 	"google.golang.org/grpc/reflection"
 	"log"
 	"net"
+	"net/http"
 )
 
-var port string
+//var port string
+//
+//func init() {
+//	flag.StringVar(&port, "p", "8000", "启动端口号")
+//	flag.Parse()
+//}
+//
+//func main() {
+//	s := grpc.NewServer()
+//	pb.RegisterTagServiceServer(s, server.NewTagServer())
+//	reflection.Register(s)
+//
+//	lis, err := net.Listen("tcp", ":"+port)
+//	if err != nil {
+//		log.Fatalf("net.Listen err: %v", err)
+//	}
+//
+//	err = s.Serve(lis)
+//	if err != nil {
+//		log.Fatalf("server.Serve err: %v", err)
+//	}
+//}
+
+var grpcPort string
+var httpPort string
 
 func init() {
-	flag.StringVar(&port, "p", "8000", "启动端口号")
+	flag.StringVar(&grpcPort, "grpc_port", "8001", "gRPC 启动端口号")
+	flag.StringVar(&httpPort, "http_port", "9001", "HTTP 启动端口号")
 	flag.Parse()
 }
 
 func main() {
+	errs := make(chan error)
+	go func() {
+		err := RunHttpServer(httpPort)
+		if err != nil {
+			errs <- err
+		}
+	}()
+	go func() {
+		err := RunGrpcServer(grpcPort)
+		if err != nil {
+			errs <- err
+		}
+	}()
+
+	select {
+	case err := <-errs:
+		log.Fatalf("Run Server err: %v", err)
+	}
+}
+
+func RunHttpServer(port string) error {
+	serveMux := http.NewServeMux()
+	serveMux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte(`pong`))
+	})
+
+	return http.ListenAndServe(":"+port, serveMux)
+}
+
+func RunGrpcServer(port string) error {
 	s := grpc.NewServer()
 	pb.RegisterTagServiceServer(s, server.NewTagServer())
 	reflection.Register(s)
-
 	lis, err := net.Listen("tcp", ":"+port)
 	if err != nil {
-		log.Fatalf("net.Listen err: %v", err)
+		return err
 	}
 
-	err = s.Serve(lis)
-	if err != nil {
-		log.Fatalf("server.Serve err: %v", err)
-	}
+	return s.Serve(lis)
 }
